2505869a3e0b74ddcd1f5fd57cc3e679282e2b7e
Bruno GANTELMI <bgantelm@e1r5p19.42.fr>
2016-05-05 16:28:11 +0200

    ajout

---
+++ SlideMenuControllerSwift/MainViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  SlideMenuControllerSwift
+//
+//  Created by Yuji Hato on 12/3/14.
+//
+
+import UIKit
+import MapKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var MyMAp: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillTransitionToSize(size: CGSize, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransitionToSize(size, withTransitionCoordinator: coordinator)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.setNavigationBarItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension MainViewController : SlideMenuControllerDelegate {
+    
+    func leftWillOpen() {
+        print("SlideMenuControllerDelegate: leftWillOpen")
+    }
+    
+    func leftDidOpen() {
+        print("SlideMenuControllerDelegate: leftDidOpen")
+    }
+    
+    func leftWillClose() {
+        print("SlideMenuControllerDelegate: leftWillClose")
+    }
+    
+    func leftDidClose() {
+        print("SlideMenuControllerDelegate: leftDidClose")
+    }
+    
+    func rightWillOpen() {
+        print("SlideMenuControllerDelegate: rightWillOpen")
+    }
+    
+    func rightDidOpen() {
+        print("SlideMenuControllerDelegate: rightDidOpen")
+    }
+    
+    func rightWillClose() {
+        print("SlideMenuControllerDelegate: rightWillClose")
+    }
+    
+    func rightDidClose() {
+        print("SlideMenuControllerDelegate: rightDidClose")
+    }
+}
